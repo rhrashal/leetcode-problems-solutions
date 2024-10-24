71ef920721929348af2acc303e0ea534678e6a37
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-10-24 14:27:54 +0600

    Update 0196.Delete-Duplicate-Emails.sql

--- 0196.Delete-Duplicate-Emails.sql
+++ 0196.Delete-Duplicate-Emails.sql
@@ -1,4 +1,24 @@
-   WITH cte AS (
+-- Create a temporary table
+CREATE TABLE person (
+    id INT,
+    email VARCHAR(255)
+);
+
+-- Insert data into the temporary table
+INSERT INTO person (id, email)
+VALUES
+(1, 'john@example.com'),
+(2, 'bob@example.com'),
+(3, 'john@example.com');
+
+-- Select the data to verify the insertion
+SELECT * FROM person;
+
+
+
+--################ 
+
+WITH cte AS (
     SELECT  email , 
     ROW_NUMBER() OVER (PARTITION BY email ORDER BY id ) row_num
      FROM 
