8be1057db60503ce5cb968b3b2e32283ec3c883b
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-10-24 15:01:14 +0600

    Update 0183.Customers-Who-Never-Order.sql

--- 0183.Customers-Who-Never-Order.sql
+++ 0183.Customers-Who-Never-Order.sql
@@ -1,11 +1,11 @@
 -- Create a temporary table for Customers
-CREATE TABLE customers (
+CREATE TABLE #customers (
     id INT,
     name VARCHAR(255)
 );
 
 -- Insert data into the Customers table
-INSERT INTO customers (id, name)
+INSERT INTO #customers (id, name)
 VALUES
 (1, 'Joe'),
 (2, 'Henry'),
@@ -13,32 +13,33 @@ VALUES
 (4, 'Max');
 
 -- Create a temporary table for Orders
-CREATE  TABLE orders (
+CREATE  TABLE #orders (
     id INT,
     customerId INT
 );
 
 -- Insert data into the Orders table
-INSERT INTO orders (id, customerId)
+INSERT INTO #orders (id, customerId)
 VALUES
 (1, 3),
 (2, 1);
 
 -- Select the data from both tables to verify the insertion
-SELECT * FROM customers;
-SELECT * FROM orders;
+SELECT * FROM #customers;
+SELECT * FROM #orders;
 
 
 --solve 1 #####################################
 select c.name as Customers 
-from Customers c
-left join orders o on c.id = o.customerId
+from #Customers c
+left join #orders o on c.id = o.customerId
 where o.Id is null
 
 --solve 2 #####################################    
-select name as Customers from Customers where id not in (select customerId from orders)
+select name as Customers from #Customers where id not in (select customerId from #orders)
 
 
 
 
-
+drop Table #customers
+drop Table #orders
