e21c1be9d6ca8943cb20f62257022b02b41a4cac
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-10-24 14:25:51 +0600

    Update 0177.Nth-Highest-Salary.sql

--- 0177.Nth-Highest-Salary.sql
+++ 0177.Nth-Highest-Salary.sql
@@ -1,4 +1,27 @@
 
+-- Create a temporary table for Employee
+CREATE TABLE employee (
+    id INT,
+    name VARCHAR(255),
+    salary DECIMAL(10, 2),
+    managerId INT
+);
+
+-- Insert data into the temporary table
+INSERT INTO employee (id, name, salary, managerId)
+VALUES
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, NULL),
+(4, 'Max', 90000, NULL);
+
+-- Select the data to verify the insertion
+SELECT * FROM employee;
+
+
+-- ###############SOLVE######################
+
+
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
