55e93e3be1cacf204c69e6f7bb2f5e37fe254bd5
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-10-24 15:03:29 +0600

    Update 0182.Duplicate-Emails.sql

--- 0182.Duplicate-Emails.sql
+++ 0182.Duplicate-Emails.sql
@@ -1,23 +1,26 @@
 -- Create a temporary table
-CREATE TABLE person (
+CREATE TABLE #person (
     id INT,
     email VARCHAR(255)
 );
 
 -- Insert data into the temporary table
-INSERT INTO person (id, email)
+INSERT INTO #person (id, email)
 VALUES
 (1, 'john@example.com'),
 (2, 'bob@example.com'),
 (3, 'john@example.com');
 
 -- Select the data to verify the insertion
-SELECT * FROM person;
+SELECT * FROM #person;
 
 
 --#######################
 
 
-select email    from Person 
+select email    from #Person 
 group by email
 having count(email)>1
+
+
+drop table #Person
