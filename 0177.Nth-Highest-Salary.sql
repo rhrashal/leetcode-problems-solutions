4ffd59eb27959555b84a50d44809a001b57ad5a4
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-10-24 15:29:41 +0600

    Update 0177.Nth-Highest-Salary.sql

--- 0177.Nth-Highest-Salary.sql
+++ 0177.Nth-Highest-Salary.sql
@@ -1,33 +1,41 @@
-
--- Create a temporary table for Employee
+-- Create a temporary table for Employee in SQL Server
 CREATE TABLE employee (
     id INT,
-    name VARCHAR(255),
-    salary DECIMAL(10, 2),
-    managerId INT
+    salary DECIMAL(10, 2)
 );
 
--- Insert data into the temporary table
-INSERT INTO employee (id, name, salary, managerId)
+-- Insert data into the temporary Employee table
+INSERT INTO employee (id, salary)
 VALUES
-(1, 'Joe', 70000, 3),
-(2, 'Henry', 80000, 4),
-(3, 'Sam', 60000, NULL),
-(4, 'Max', 90000, NULL);
+(1, 100),
+(2, 200),
+(3, 300);
 
 -- Select the data to verify the insertion
 SELECT * FROM employee;
 
 
--- ###############SOLVE######################
+go
 
-
-CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+CREATE FUNCTION getNthHighestSalary(@N INT) 
+RETURNS INT AS
 BEGIN
     RETURN (
         select max(a.salary ) from 
-(select salary , dense_rank() over (order by salary  desc) as rn
-from Employee ) a
-where a.rn = @N
+		(select salary , dense_rank() over (order by salary  desc) as rn
+		from employee ) a
+		where a.rn = @N
     );
+
 END
+
+go
+-- Call the function and get the Nth highest salary
+SELECT dbo.getNthHighestSalary(1) AS SecondHighestSalary;
+
+
+
+go
+drop function getNthHighestSalary
+go
+drop table employee
