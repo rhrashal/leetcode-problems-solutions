511dd72c48ec0a75190a61f8b6ec6abd317ef058
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-05 18:05:43 +0600

    Create 2620.Counter.js

---
+++ JS/2620.Counter.js
@@ -0,0 +1,11 @@
+function createCounter(start) {
+  let count = start;
+  return function() {
+    return count++;
+  }
+}
+
+const counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
