31b97efd6bfe6312031191f873c9368b55ad72cc
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-05 18:20:17 +0600

    Create 2665.Counter-II.js

---
+++ JS/2665.Counter-II.js
@@ -0,0 +1,24 @@
+var createCounter = function(init) {
+    let count = init;
+    function increment() {
+        return ++count;
+    }
+    function decrement() {
+        return --count;
+    }
+    function reset() {
+        count = init;
+        return count;
+    }
+    return {
+        increment,
+        decrement,
+        reset
+    };
+};
+
+
+const counter = createCounter(5);
+console.log(counter.increment());  // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
