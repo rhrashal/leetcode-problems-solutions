a3e449869ee7a090ebc5f2306e9fbd6df947c362
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-09 23:55:29 +0600

    Update 1068.Product-Sales-Analysis-I.sql

--- SQL/1068.Product-Sales-Analysis-I.sql
+++ SQL/1068.Product-Sales-Analysis-I.sql
@@ -31,6 +31,26 @@ VALUES
 SELECT p.product_name,s.year,s.price FROM #sales s
 inner join #product p on s.product_id = p.product_id
 
+    
+-- using CTE
+WITH SaleDetails AS (
+    SELECT 
+        s.sale_id,
+        p.product_name,
+        s.year,
+        s.price
+    FROM 
+        #sales s
+    JOIN 
+        #product p ON s.product_id = p.product_id
+)
+SELECT 
+    product_name,
+    year,
+    price
+FROM 
+    SaleDetails;    
+
 
 
 drop table #sales
