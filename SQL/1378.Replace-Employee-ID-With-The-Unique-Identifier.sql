fc9637cde07bcf699cb437267f440978aa0e896b
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-09 23:41:55 +0600

    Update 1378.Replace-Employee-ID-With-The-Unique-Identifier.sql

--- SQL/1378.Replace-Employee-ID-With-The-Unique-Identifier.sql
+++ SQL/1378.Replace-Employee-ID-With-The-Unique-Identifier.sql
@@ -33,7 +33,25 @@ VALUES
 SELECT u.unique_id,e.name FROM #employees e
 left join  #employeeuni u on e.id = u.id
 
+--#Approach 1: Basic Left Join
+ SELECT EmployeeUNI.unique_id ,#employees.name FROM 
+ #employees LEFT JOIN #employeeuni 
+ ON #employees.id=#employeeuni.id;
 
+--#Approach 2: Switch Case
+ SELECT
+     CASE
+         WHEN #employeeuni.unique_id IS NULL THEN null
+         ELSE #employeeuni.unique_id
+     END AS unique_id,
+     #employees.name
+ FROM #employees
+ LEFT JOIN #employeeuni ON #employees.id = #employeeuni.id;
+
+--#Approach 3 :COALESCE
+ SELECT COALESCE(#employeeuni.unique_id, null) AS unique_id, #employees.name
+ FROM #employees
+ LEFT JOIN #employeeuni ON #employees.id = #employeeuni.id;
 
 
 DROP TABE #employees;
