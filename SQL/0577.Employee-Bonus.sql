e6eb3d883c36227b5857bd00c621a2964882829a
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-12 17:42:03 +0600

    Update 0577.Employee-Bonus.sql

--- SQL/0577.Employee-Bonus.sql
+++ SQL/0577.Employee-Bonus.sql
@@ -35,8 +35,12 @@ select e.name,b.bonus from #employee e
 left join #bonus b on e.empId = b.empId
 where b.bonus < 1000
 OR b.bonus is null
-
-
+--or
+SELECT e.name, b.bonus
+FROM #Employee e
+LEFT JOIN #Bonus b
+ON e.empId=b.empId
+WHERE ISNULL(bonus, 0)<1000
 
 
 drop table #employee;
