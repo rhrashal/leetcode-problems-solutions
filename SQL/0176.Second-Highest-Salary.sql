f07ad357cdf581f67766e62f15e639f2026f66f6
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-11-23 13:44:21 +0600

    Update 0176.Second-Highest-Salary.sql

--- SQL/0176.Second-Highest-Salary.sql
+++ SQL/0176.Second-Highest-Salary.sql
@@ -17,12 +17,23 @@ SELECT * FROM #employee;
 
 
 go
-
+-- sol -1
 Select MAX(salary) AS "SecondHighestSalary"
 From #Employee
 WHERE salary <(SELECT MAX(salary) From #Employee)
 go
 
+    -- sol -2
+WITH cte AS (
+				SELECT e.salary, ROW_NUMBER() OVER (PARTITION BY 1 ORDER BY salary DESC) AS row_num 
+				FROM ( SELECT DISTINCT salary FROM #Employee ) e 
+			) 
+			SELECT 
+			CASE WHEN EXISTS (SELECT 1 FROM cte WHERE row_num = 2) 
+			THEN (SELECT salary FROM cte WHERE row_num = 2)
+			ELSE NULL END AS salary;
+
+go
 
 
 drop table #employee
