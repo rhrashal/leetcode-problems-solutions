f6e931c2743dd0b1281b00f581e13893704d98d9
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-12-05 14:31:39 +0600

    Create 0058.Length-of-Last-Word.cs

---
+++ CSharp/0058.Length-of-Last-Word.cs
@@ -0,0 +1,29 @@
+using System;
+using static System.Math;
+using static System.Console;
+using ConsoleApp1;
+using System.Xml.Linq;
+
+class program
+{
+    public static int LengthOfLastWord2(string s)
+    {
+        return s.Trim().Split(' ').Last().Length;
+    }
+    public static int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' ').Where(e => e.Length > 0).ToArray();
+        return words[words.Length - 1].Length;
+    }
+    static void Main()
+    {
+        string input = "   fly me   to   the moon  ";
+        Console.WriteLine(LengthOfLastWord(input));        
+    }
+}
+
+
+
+
+
+
