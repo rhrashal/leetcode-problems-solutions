8dcb18a2bb514745fefaf0c39c9a649f56c79acc
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2024-12-12 17:19:30 +0600

    Create 2627.Debounce.js

---
+++ JS/2627.Debounce.js
@@ -0,0 +1,16 @@
+var debounce = function(fn, t) {
+    let timeout = null;
+    
+    return function(...args) {
+        if(timeout)
+            clearTimeout(timeout);
+        timeout = setTimeout(() => fn(...args), t);
+    }
+};
+
+
+  const log = debounce(console.log, 100);
+  log('Hello'); // cancelled
+  log('Hello'); // cancelled
+  log('Hello'); // Logged at t=100ms
+ 
